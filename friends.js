0ace7307fa1b2b4ea532dc95451c00e2831f5abb
Joshua Martin <ursa8minority@gmail.com>
2022-11-10 18:15:30 -0700

    Complete second challenge

--- friends.js
+++ friends.js
@@ -4,3 +4,11 @@ for (var i = 0; i <= numbers.length; i++) {
         console.log(numbers[i]);
     }
 }
+//
+
+var numbers = [2, 35, 40, 50, 66, 72, 31, 99]
+for (var i = 0; i <= numbers.length; i++) {
+    if (numbers[i] < 60) {
+        console.log(numbers[i]);
+    }
+}
